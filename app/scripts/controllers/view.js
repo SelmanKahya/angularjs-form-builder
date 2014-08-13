50b34671d5dedc4d24cc7181d9d737da0ed0f381
Selman K <selmanhalid@hotmail.com>
2014-08-13 08:57:58 -0700

    making some changes you wouldn't care

--- app/scripts/controllers/view.js
+++ app/scripts/controllers/view.js
@@ -2,9 +2,8 @@
 
 var ViewCtrl = angularApp.controller('ViewCtrl', function ($scope, FormService, $routeParams) {
     $scope.form = {};
-
-    // read form with given id
-    FormService.form($routeParams.id).then(function(form) {
-        $scope.form = form;
-    });
+	// read form with given id
+	FormService.form($routeParams.id).then(function(form) {
+		$scope.form = form;
+	});
 });
